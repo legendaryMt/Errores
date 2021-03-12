2dee9a0fd34a0a392477bf38800c734ba0336753
EQUIPO 01 <EQUIPO 01@DESKTOP-5BC9723>
2021-03-11 18:30:36 -0600

    EjerciciosUnidad1

---
+++ src/ErrorRelativoPorcentual.java
@@ -0,0 +1,17 @@
+
+public class ErrorRelativoPorcentual {
+        private double ValorReal;
+        private double ValorAproximado;
+         public void setValorReal(double VR){
+            ValorReal=VR;
+        }
+        public double getValorReal(){
+            return ValorReal;
+        }
+         public void setValorAproximado(double VA){
+            ValorReal=VA;
+        }
+        public double getValorAproximado(){
+            return ValorAproximado;
+        }
+}
